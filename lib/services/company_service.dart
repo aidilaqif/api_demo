07002d646059112c0984c7c0f408210178a1a747
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2024-10-30 13:12:02 +0800

    #6 decode data

--- lib/services/company_service.dart
+++ lib/services/company_service.dart
@@ -1,14 +1,24 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+
+import '../model/company.dart';
 class CompanyService{
 
   String baseUrl = "https://retoolapi.dev/y4lflW/";
 
   getAllCompanies() async {
     try {
+      List<Company> allCompanies = [];
       var response = await http.get(Uri.parse(baseUrl+'company'));
 
       if(response.statusCode == 200){
         var data = response.body;
+
+        var jsonData = jsonDecode(data);
+
+        for (var company in jsonData){
+          Company newCompany = Company.fromJson(company);
+        }
       }else{
         throw Exception("Error occured with status code ${response.statusCode} and the message is ${response.body}");
       }

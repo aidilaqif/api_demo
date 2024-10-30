92a1855b623f54a9229cad229cd2a4963532ad53
AIDIL 'AQIF <92839227+aidilaqif@users.noreply.github.com>
2024-10-30 13:03:20 +0800

    Update company_service.dart

--- lib/services/company_service.dart
+++ lib/services/company_service.dart
@@ -1,18 +1,38 @@
+import 'package:http/http.dart' as http;
 class CompanyService{
 
-  getAllCompanies() async {
+  String baseUrl = "https://retoolapi.dev/y4lflW/";
 
+  getAllCompanies() async {
+    try {
+      var response = await http.get(Uri.parse(baseUrl+'company'));
+    } catch (e) {
+      print("Error occured: ${e}");
+    }
   }
 
   createCompany() async {
+    try {
+      
+    } catch (e) {
+      print("Error occured: ${e}");
+    }
 
   }
 
   updateCompany() async {
-
+    try {
+      
+    } catch (e) {
+      print("Error occured: ${e}");
+    }
   }
 
   deleteCompany() async {
-
+    try {
+      
+    } catch (e) {
+      print("Error occured: ${e}");
+    }
   }
 }
